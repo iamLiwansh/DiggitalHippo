c7c83d9ed23f6436c89094d28ccb86f73dfb22ac
iamLiwansh <saini.liwansh@gmail.com>
2024-03-21 14:20:00 +0530

    updated Navbar Categories

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
+import { Icons } from "./Icons"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import Link from 'next/link'
+import NavItems from "./NavItems"
 const Navbar = () =>{
     return (
         <div className="bg-white sticky z-50 top-0 inset-x h-16">
@@ -10,7 +12,12 @@ const Navbar = () =>{
                             {/*Todo : mobile nav */}
 
                             <div className="ml-4 flex lg:ml-0">
-                                <Link href="/"></Link>
+                                <Link href="/">
+                                    <Icons.logo className='h-10 w-10'/>
+                                </Link>
+                            </div>
+                            <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
+                                <NavItems/>
                             </div>
                         </div>
                     </div>

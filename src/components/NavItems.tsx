aee11ddac3b714dc318152602b243084a7b2a484
iamLiwansh <saini.liwansh@gmail.com>
2024-03-25 13:53:38 +0530

    updated navbar

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,17 +1,39 @@
 "use client"
 
 import { PRODUCT_CATEGORIES } from "@/config"
-import { useState } from "react"
+import { KeyboardEvent, useEffect, useRef, useState } from "react"
 import NavItem from "./Navitem"
+import { useOnClickOutside } from "@/hooks/use-on-click-outside"
 
 const NavItems = () => {
 
     const [activeIndex , setActiveIndex] = useState<null | number>(null)
 
-    const isAnyOpen = activeIndex !== null //null ko chrd ke kuch bhi value hui 
+
+    useEffect(() => {
+        const handler: EventListenerOrEventListenerObject = (e: Event) => {
+            if ((e as unknown as KeyboardEvent).key === "Escape") {
+                setActiveIndex(null); // close the navbar when the ESC key is pressed
+            }
+        };
+    
+        document.addEventListener("keydown", handler);
+    
+        // Cleanup function to remove the event listener when component unmounts
+        return () => {
+            document.removeEventListener("keydown", handler);
+        };
+    }, []); 
+    
+
+    const isAnyOpen = activeIndex !== null //null ko chrd ke kuch bhi value hui (isAnyOpen koi or navbar mai khula hua hai kya)
+
+    const navRef = useRef<HTMLDivElement | null>(null)
+
+    useOnClickOutside(navRef,() => setActiveIndex(null))
 
     
-    return <div className=" flex gap-4 h-full">
+    return <div className=" flex gap-4 h-full" ref={navRef}>
 
         {PRODUCT_CATEGORIES.map((category,i) => {
             const handleOpen = () => {

2c3a7940eafc730e46aed5c86dbff9e2b73b1572
iamLiwansh <saini.liwansh@gmail.com>
2024-03-26 14:43:21 +0530

    added nodemon

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import { getPayloadClient } from "./get-payload"
+import { nextApp, nextHandler } from "./next-utils"
 
 
 const app = express()
@@ -13,6 +14,15 @@ const start = async () => {
                 cms.logger.info(`admin URL ${cms.getAdminURL()}`)
             }
         }
+    })
+    app.use((req,res) => nextHandler(req, res))
+
+    nextApp.prepare().then(()=>{
+        payload.logger.info('Next.js Started')
+
+        app.listen(PORT, async () => {
+            payload.logger.info(`Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`)
+        })
     })
 }
 

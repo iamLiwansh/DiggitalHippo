190779891c6537486fd3cce9eddf3befa7c1ea0b
iamLiwansh <saini.liwansh@gmail.com>
2024-03-26 13:35:30 +0530

    updated admin dashboard

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv"//dotenv used for storing senstive data or ye github pe push bhi nhi hota
 import path from "path"
+import type {InitOptions} from "payload/config"
+import payload from"payload"
 
 dotenv.config({
     path: path.resolve(__dirname,"../.env")//take this directory=>(__dirname says current dircotory) then go back to ../ directory and find .env 
@@ -14,4 +16,32 @@ if(!cached){
         promise:null,
     }
 }//agar nhi hai
-export const getPlayloadClient = async () =>  {}
+
+interface Args{
+    initOptions?: Partial<InitOptions>
+}
+export const getPayloadClient = async ({initOptions}: Args ={}) =>  {
+    if(!process.env.PAYLOAD_SECRET){
+        throw new Error('PAYLOAD_SECRET is missing')
+    }
+    if(cached.client){
+        return cached.client
+    }
+    if(!cached.promise){
+        cached.promise =payload.init({
+            secret:process.env.PAYLOAD_SECRET,
+            local: initOptions?.express ? false : true,
+            ...(initOptions || {}),
+        })
+    }
+    try{
+        cached.client = await cached.promise
+    } catch(e : unknown){
+        cached.promise = null
+        throw e
+    }
+
+    return cached.client
+}
+//overview - we created a dbclient now we can use in entire app and we also store to cache the info.
+//we can directly use getPayload for dbclient
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import express from "express"
+import { getPayloadClient } from "./get-payload"
+
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
 
 const start = async () => {
-    const payload = await getPayloadClient()//this is the admin dashboard statup(we can also use a db client to fetch or add data into the database)
+    const payload = await getPayloadClient({//this is the admin dashboard statup(we can also use a db client to fetch or add data into the database)
+        initOptions:{
+            express: app,
+            onInit: async (cms) => {
+                cms.logger.info(`admin URL ${cms.getAdminURL()}`)
+            }
+        }
+    })
 }
 
 start()
